b7d8137b42c120b5f7a263f1d95b5d874941d05b
Neopentene <celestynkinny3@gmail.com>
2022-05-01 18:16:03 +0530

    main application commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,141 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header
+    class="card logo"
+    style="display: flex; justify-content: center; align-items: center"
+  >
+    <h1 style="margin: 0">Diffie Hellman Algorithm</h1>
+  </header>
+  <Algorithm></Algorithm>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Algorithm from './components/Algorithm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Algorithm
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  outline-color: #606c38;
 }
+
+:root {
+  --color-light: #fefae0;
+  --color-dark: #283618;
+  --color-signal: #bc6c25;
+  --color-background: var(--color-light);
+  --color-text: var(--color-dark);
+  --color-accent: var(--color-signal);
+  --size-bezel: 0.5rem;
+  --size-radius: 4px;
+  line-height: 1.4;
+  font-family: "Inter", sans-serif;
+  font-size: calc(0.6rem + 0.4vw);
+  color: var(--color-text);
+  background: var(--color-background);
+  font-weight: 300;
+  padding: 0 calc(var(--size-bezel) * 3);
+}
+
+h1,
+h2,
+h3,
+h4 {
+  font-weight: 900;
+}
+
+mark {
+  background: var(--color-accent);
+  color: var(--color-text);
+  font-weight: bold;
+  padding: 0 0.2em;
+}
+
+.card {
+  width: 100%;
+  background: var(--color-background);
+  padding: calc(4 * var(--size-bezel));
+  margin-top: calc(4 * var(--size-bezel));
+  border-radius: var(--size-radius);
+  border: 3px solid var(--color-shadow, currentColor);
+  box-shadow: 0.5rem 0.5rem 0 var(--color-shadow, currentColor);
+  box-sizing: border-box;
+}
+
+.card *:first-child {
+  margin-top: 0;
+}
+
+.l-design-widht {
+  max-width: 40rem;
+  padding: 1rem;
+}
+
+.card.simple {
+  max-width: 70rem;
+}
+
+.input {
+  position: relative;
+}
+
+.input__label {
+  position: absolute;
+  left: 0;
+  top: 0;
+  padding: calc(var(--size-bezel) * 0.75) calc(var(--size-bezel) * 0.5);
+  margin: calc(var(--size-bezel) * 0.75 + 3px) calc(var(--size-bezel) * 0.5);
+  background: pink;
+  white-space: nowrap;
+  transform: translate(0, 0);
+  transform-origin: 0 0;
+  background: var(--color-background);
+  transition: transform 120ms ease-in;
+  font-weight: bold;
+  line-height: 1.2;
+}
+
+.input__field {
+  box-sizing: border-box;
+  display: block;
+  width: 100%;
+  border: 3px solid currentColor;
+  padding: calc(var(--size-bezel) * 1.5) var(--size-bezel);
+  color: currentColor;
+  background: transparent;
+  border-radius: var(--size-radius);
+}
+
+.input__field:not(:-moz-placeholder-shown) + .input__label {
+  transform: translate(0.25rem, -65%) scale(0.8);
+  color: var(--color-accent);
+}
+
+.input__field:not(:-ms-input-placeholder) + .input__label {
+  transform: translate(0.25rem, -65%) scale(0.8);
+  color: var(--color-accent);
+}
+
+.input__field:focus + .input__label,
+.input__field:not(:placeholder-shown) + .input__label {
+  transform: translate(0.25rem, -65%) scale(0.8);
+  color: var(--color-accent);
+}
+
+#app {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+#app:last-child {
+  margin-bottom: 2rem;
+}
+
 </style>
---
+++ src/components/Algorithm.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="card simple">
+    <h2>Input (📝)</h2>
+    <h3>Enter the Value of P (<code>🙏</code>)</h3>
+    <label class="input">
+      <input
+        class="input__field"
+        type="number"
+        placeholder=" "
+        name="P"
+        v-model="P"
+      />
+      <span class="input__label">Enter the Value of P</span>
+    </label>
+    <h3>Enter the Value of G (<code>🙏</code>)</h3>
+    <label class="input">
+      <input
+        class="input__field"
+        type="number"
+        placeholder=" "
+        name="G"
+        v-model="G"
+      />
+      <span class="input__label">Enter the Value of G</span>
+    </label>
+    <h3>Enter private key for Alice (<code>a 🕶</code>)</h3>
+    <label class="input">
+      <input
+        class="input__field"
+        type="number"
+        placeholder=" "
+        name="a"
+        v-model="a"
+      />
+      <span class="input__label">Enter private key for Alice</span>
+    </label>
+    <h3>Enter private key for Bob (<code>b 🕶</code>)</h3>
+    <label class="input">
+      <input
+        class="input__field"
+        type="number"
+        placeholder=" "
+        name="b"
+        v-model="b"
+      />
+      <span class="input__label">Enter private key for Bob</span>
+    </label>
+  </div>
+  <div class="card simple">
+    <h2>Output (📤)</h2>
+    <h3>
+      Public value for Alice (<code>x = G<sup>a</sup> mod P</code>)
+    </h3>
+    <output name="public-key-alice" for="a b">
+      <h4>📢: <code>{{ setX(power(G, a, P)) }}</code></h4>
+    </output>
+    <h3>
+      Public value for Bob (<code>y = G<sup>b</sup> mod P</code>)
+    </h3>
+    <output name="public-key-alice" for="a b">
+      <h4>📢: <code>{{ setY(power(G, b, P))  }}</code></h4>
+    </output>
+    <h3>
+      Symmetric Key for Alice (<code>key-alice = y<sup>a</sup> mod P</code>)
+    </h3>
+    <output name="public-key-alice" for="a b">
+      <h4>📢: <code>{{ setKeyAlice(power(y, a, P)) }}</code></h4>
+    </output>
+    <h3>
+      Symmetric Key for Bob (<code>key-bob = x<sup>b</sup> mod P</code>)
+    </h3>
+    <output name="public-key-alice" for="a b">
+      <h4>📢: <code>{{ setKeyBob(power(x, b, P)) }}</code></h4>
+    </output>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlgorithmVue',
+  data: function () {
+    return {
+      P: Number,
+      G: Number,
+      a: Number,
+      b: Number,
+      x: Number,
+      y: Number,
+      key_alice: Number,
+      key_bob: Number
+    }
+  },
+  props: {
+    msg: String
+  },
+  methods: {
+    power (a, b, p) {
+      if (b === 1) {
+        return a
+      } else if (b === 0 || b === '') {
+        return Number.NaN
+      } else {
+        return ((Math.pow(a, b)) % p)
+      }
+    },
+    setX (val) {
+      this.x = val
+      return this.x
+    },
+    setY (val) {
+      this.y = val
+      return this.y
+    },
+    setKeyAlice (val) {
+      this.key_alice = val
+      return this.key_alice
+    },
+    setKeyBob (val) {
+      this.key_bob = val
+      return this.key_bob
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
